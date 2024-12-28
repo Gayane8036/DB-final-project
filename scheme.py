3a8843f7f268bb70ef3e6863c9693cf90c517b40
nomore <149250127+Gayane8036@users.noreply.github.com>
2024-12-28 16:45:25 +0400

    Create scheme.py

---
+++ scheme.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import List, Optional
+
+class CreatorCreate(BaseModel):
+    full_name: str
+    birth_year: int
+    death_year: Optional[int] = None
+    country: str
+    main_style: str
+
+class CreatorResponse(BaseModel):
+    id: int
+    full_name: str
+    birth_year: int
+    death_year: Optional[int]
+    country: str
+    main_style: str
+
+    class Config:
+        orm_mode = True
+
+class ArtworkCreate(BaseModel):
+    type: str
+    cost: float
+    title: str
+    dimensions: str
+    material: str
+    creator_id: int
+    storage_id: int
+
+class ArtworkResponse(BaseModel):
+    id: int
+    type: str
+    cost: float
+    title: str
+    dimensions: str
+    material: str
+    creator_id: int
+    storage_id: int
+
+    class Config:
+        orm_mode = True
+
+class StorageCreate(BaseModel):
+    name: str
+    type: str
+    country: str
+    opening_date: date
+
+class StorageResponse(BaseModel):
+    id: int
+    name: str
+    type: str
+    country: str
+    opening_date: date
+
+    class Config:
+        orm_mode = True
