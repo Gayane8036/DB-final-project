9ee3be6f1872749af7b3b5ebb78c1c7fee6ecdb4
Gayane8036 <149250127+Gayane8036@users.noreply.github.com>
2024-12-28 16:25:08 +0400

    Create db.py

---
+++ db.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+url = URL.create(
+    drivername="postgresql",
+    username="postgres",
+    password="postgres",
+    host="localhost",
+    database="postgres", 
+    port=5432
+)
+
+engine = create_engine(url)
+
+from sqlalchemy import text
+
+with engine.connect() as conn:
+    conn.execute(text("COMMIT"))
+    conn.execute(text("CREATE DATABASE artdb"))
+
+url = URL.create(
+    drivername="postgresql",
+    username="postgres",
+    password="postgres",
+    host="localhost",
+    database="artdb",  
+    port=5432
+)
+engine = create_engine(url)
+
+Base.metadata.create_all(bind=engine)
+
+print("DataBases are successfully created!")
