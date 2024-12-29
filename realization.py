750a3849d7fca8b5c58fbbcefaf08452488e35b3
nomore <149250127+Gayane8036@users.noreply.github.com>
2024-12-29 15:09:47 +0400

    Update realization.py

--- realization.py
+++ realization.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-import CRUD, scheme
+import crud, scheme 
 from database import SessionLocal, engine
 
 def get_db():
@@ -13,14 +13,14 @@ def get_db():
 
 app = FastAPI()
 
-@app.post("/creators/", response_model=schemas.Creator)
-def create_creator(creator: schemas.Creator, db: Session = Depends(get_db)):
+@app.post("/creators/", response_model=scheme.CreatorResponse) 
+def create_creator(creator: scheme.CreatorCreate, db: Session = Depends(get_db)): 
     return crud.create_creator(db=db, creator=creator)
 
-@app.post("/artworks/", response_model=schemas.Artwork)
-def create_artwork(artwork: schemas.Artwork, db: Session = Depends(get_db)):
+@app.post("/artworks/", response_model=scheme.ArtworkResponse)  
+def create_artwork(artwork: scheme.ArtworkCreate, db: Session = Depends(get_db)):
     return crud.create_artwork(db=db, artwork=artwork)
 
-@app.post("/storage_places/", response_model=schemas.StoragePlace)
-def create_storage_place(storage_place: schemas.StoragePlace, db: Session = Depends(get_db)):
+@app.post("/storage_places/", response_model=scheme.StorageResponse)
+def create_storage_place(storage_place: scheme.StorageCreate, db: Session = Depends(get_db)):
     return crud.create_storage_place(db=db, storage_place=storage_place)
