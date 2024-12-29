a8ee5f3f30d00b6ef418e26617fc870841b886f8
nomore <149250127+Gayane8036@users.noreply.github.com>
2024-12-29 14:20:53 +0400

    Update realization.py

--- realization.py
+++ realization.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-import crud, schemе
+import CRUD, scheme
 from database import SessionLocal, engine
 
 def get_db():
