d4bc53694195758203840fcab49de7ee6d8ee1f0
nomore <149250127+Gayane8036@users.noreply.github.com>
2025-01-02 14:34:08 +0400

    Update and rename models.py to app/models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import JSONB
 
 Base = declarative_base()
 
